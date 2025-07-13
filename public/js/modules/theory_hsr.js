d03d752900e590e445147439629b6053facc9e2e
NguyenTam-Wesley <ntam3032003@gmail.com>
2025-07-13 09:29:14 +0700

    add speed to hsr

---
+++ public/js/modules/theory_hsr.js
@@ -0,0 +1,9 @@
+class TheoryHSR {
+    init() {
+        // Custom logic for HSR theory pages
+        console.log('HSR Theory page initialized!');
+        // Thêm các xử lý riêng cho trang speed tại đây
+    }
+}
+
+export default new TheoryHSR(); 
