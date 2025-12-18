ab32dea9b4936418a4e64cf5f9d83f30849a8f97
NguyenTam-Wesley <ntam3032003@gmail.com>
2025-12-18 15:05:32 +0700

    add music_entry.js

---
+++ public/js/modules/music_entry.js
@@ -0,0 +1,8 @@
+import components from "../components/components.js";
+import { MusicPlayer } from './music.js';
+      
+// Initialize components
+components.init();
+      
+// Initialize page specific functionality
+new MusicPlayer();
