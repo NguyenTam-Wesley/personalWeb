6906d329915a0c6336be4b9485e100c0eda8bd0b
NguyenTam-Wesley <ntam3032003@gmail.com>
2026-01-14 23:05:04 +0700

    update some thing

--- public/js/modules/chapter_entry.js
+++ public/js/modules/chapter_entry.js
@@ -1,4 +1,4 @@
-import components from '../../components/components.js';
+import components from '../components/components.js';
 import { ChapterPage } from './chapter.js';
 
 // Initialize components
